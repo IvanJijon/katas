80679354bd4d42dadb971d18b00f4cdf921efe30
Ivan Jijon <ivanjijon@autonomia.digital>
2023-04-11 01:01:00 -0500

    fixing test description

--- templates/ts/test/kata.spec.ts
+++ templates/ts/test/kata.spec.ts
@@ -2,7 +2,7 @@ import * as assert from "assert";
 import { alwaysReturnTrue } from "../kata/kata.js";
 
 describe('Dummy test to check if the setup is correct', () => {
-    it('should check if alwaysReturnTrue() returns true', done => {
+    it('alwaysReturnTrue() should return true', done => {
         assert.strictEqual(alwaysReturnTrue(), true);
         done();
     });
